3839ff0e2a9512ae476ea6939ca749f8d34c4870
ERICK1800 <ericklimaflorencio@gmail.com>
2023-06-19 23:43:15 -0300

    frontend

--- src/main/java/com/fatec/fornecedores/resources/FornecController.java
+++ src/main/java/com/fatec/fornecedores/resources/FornecController.java
@@ -1,5 +1,66 @@
 package com.fatec.fornecedores.resources;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.fatec.fornecedores.entities.Fornec;
+import com.fatec.fornecedores.services.FornecService;
+
+@RestController
+@RequestMapping("fornecedores")
+@CrossOrigin
 public class FornecController {
     
+    @Autowired
+    private FornecService fornecService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<Fornec> getFornec(@PathVariable int id){
+        Fornec fornec = fornecService.getFornecById(id);
+        return ResponseEntity.ok().body(fornec);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Fornec>> getFornecs(){
+        List<Fornec> fornecs = fornecService.getFornecs();
+        return ResponseEntity.ok().body(fornecs);
+    }
+
+    @PostMapping
+    public ResponseEntity<Fornec> serveFornec(@RequestBody Fornec fornec){
+        Fornec newFornec = fornecService.save(fornec);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(fornec.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(newFornec);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> editFornec(@PathVariable int id, @RequestBody Fornec fornec){
+        fornecService.update(id, fornec);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteFornec(@PathVariable int id){
+        fornecService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/fatec/fornecedores/services/FornecService.java
+++ src/main/java/com/fatec/fornecedores/services/FornecService.java
@@ -1,5 +1,43 @@
 package com.fatec.fornecedores.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fatec.fornecedores.entities.Fornec;
+import com.fatec.fornecedores.repositories.FornecRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
 public class FornecService {
+
+    @Autowired
+    private FornecRepository fornecRepository;
+
+    public Fornec getFornecById(int id) {
+        return fornecRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Fornecedor não encontrado!!!")
+        );
+    }
+
+    public List<Fornec> getFornecs() {
+        return fornecRepository.findAll();
+    }
+
+    public Fornec save(Fornec fornec) {
+        return fornecRepository.save(fornec);
+    }
+
+    public void update(int id, Fornec fornec) {
+        getFornecById(id);
+        fornecRepository.save(fornec);
+    }
+
+    public void deleteById(int id) {
+        Fornec fornec = getFornecById(id);
+        fornecRepository.delete(fornec);
+    }
     
 }
